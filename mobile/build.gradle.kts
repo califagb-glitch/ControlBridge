254616cfc0f76c01f7014e85456a516d5915f400
califagb-glitch <gabrielzincalifa@gmail.com>
2026-09-14 22:43:32 -0300

    Fix mobile Gradle source set

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.controlbridge.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
 
     compileOptions {
@@ -21,11 +21,6 @@ android {
     }
 
     kotlinOptions { jvmTarget = "17" }
-
-    sourceSets["main"].java {
-        exclude("**/MainActivity.kt")
-        exclude("**/MainActivityFixed.kt")
-    }
 }
 
 dependencies {
